6a7582385ed2c06cc6a9bd6297e3c675ee6deb2a
MansoorCM <manzoorcm96@gmail.com>
2024-12-23 12:24:56 +0530

    Take a base URL to crawl from command line.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	args := os.Args
 	fmt.Println("Web Crawler!")
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	fmt.Println("starting crawl of: ", args[1])
 }
